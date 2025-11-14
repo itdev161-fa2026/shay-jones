aab8922afc72d2a3589f371f65f7cce2aece5323
shayjones20 <jonem245@gmatc.matc.com>
2025-11-13 20:08:22 -0600

    Fix header and other style issues

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -33,7 +33,7 @@ api.interceptors.response.use(
 
 export const registerUser = async (name, email, password) => {
   try {
-    const response = await api.post('/user', { name, email, password });
+    const response = await api.post('/users', { name, email, password });
     return response.data;
   } catch (error) {
     console.error('Error registering user: ', error);
@@ -43,7 +43,7 @@ export const registerUser = async (name, email, password) => {
 
 export const loginUser = async (email, password) => {
     try {
-        const response = await api.post('/auth', {email, password }); 
+        const response = await api.post('/auth', { email, password }); 
     return response.data;
   } catch (error) {
     console.error('Error logging in user: ', error);
@@ -63,15 +63,15 @@ export const getPosts = async () => {
 };
 
 export const getPostById = async (id) => {
-     try{
-        const response = await axios.get(`${API_URL}/posts/${id}`);
-        return response.data;  
-    } 
-        catch(error) {
-            console.error('Error fetching posts:', error);
-            throw error;
-}
+  try {
+    const response = await api.get(`/posts/${id}`);
+    return response.data;  
+  } catch(error) {
+    console.error('Error fetching post by ID:', error);
+    throw error;
+  }
 };
+
 
 export const createPost = async (title, body) => {
   try {
